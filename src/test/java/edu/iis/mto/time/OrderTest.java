8565c45887cf1fd6e6926c8ea86dda5e9b519833
Paxon96 <bwloczyk@gmail.com>
2019-04-13 18:28:27 +0200

    implement test method

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,5 +1,20 @@
 package edu.iis.mto.time;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class OrderTest {
 
+    private Order order;
+
+    @Before
+    public void init(){
+        order = new Order();
+    }
+
+    @Test(expected = OrderExpiredException.class)
+    public void orderConfirmShouldThrowOrderExpiredExceprionTest(){
+        order.submit();
+        order.confirm();
+    }
 }
